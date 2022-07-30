81ff35e01cc1c5d592aebe060d6ec829ff4400ac
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-29 21:18:24 -0300

    feat: Car Controller, method: create

---
+++ src/controllers/Car.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import IService from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) {}
+
+  public async create(req: Request & { body: ICar }, res: Response<ICar>) {
+    const { body: car } = req;
+    const results = await this._service.create(car);
+    return res.status(201).json(results);
+  }
+}

39636561eb66d5e8ef9b58f3aecd1f3abae4a6ef
R-R-Freitas <rodrigoaz8@gmail.com>
2022-07-30 01:23:43 -0300

    fix: corrected field name in errorHandler returns(from 'message' to 'error')

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -9,7 +9,7 @@ const errorHandler: ErrorRequestHandler = (
   _next,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({ message: err.issues });
+    return res.status(400).json({ error: err.issues });
   }
 
   const messageAsErrorType = err.message as keyof typeof ErrorTypes;
@@ -17,9 +17,9 @@ const errorHandler: ErrorRequestHandler = (
 
   if (mappedError) {
     const { httpStatus, message } = mappedError;
-    return res.status(httpStatus).json({ message });
+    return res.status(httpStatus).json({ error: message });
   }
-  return res.status(500).json({ message: err.message });
+  return res.status(500).json({ error: err.message });
 };
 
 export default errorHandler;

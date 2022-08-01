21082f0159a20ac45ad1f676088c4b3dc8e80aff
R-R-Freitas <rodrigoaz8@gmail.com>
2022-08-01 18:13:07 -0300

    feat: Motorcycle model

---
+++ src/models/Motorcycle.ts
@@ -0,0 +1,26 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './mongoModel';
+
+const motorcycleMongooseSchema = new Schema<IMotorcycle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  category: String,
+  engineCapacity: Number,
+}, {
+  // source: https://stackoverflow.com/questions/13699784/mongoose-v-property-hide
+  versionKey: false,
+});
+
+class Motorcycle extends MongoModel<IMotorcycle> {
+  constructor(
+    model = mongooseCreateModel('Motorcycle', motorcycleMongooseSchema),
+  ) {
+    super(model);
+  }
+}
+
+export default Motorcycle;

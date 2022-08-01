a7e26f4c92cdd143f6b2172841c1cc0a47ea4219
R-R-Freitas <rodrigoaz8@gmail.com>
2022-08-01 19:45:31 -0300

    refactor: added path const to car routes

--- src/routes/car.ts
+++ src/routes/car.ts
@@ -4,15 +4,16 @@ import CarService from '../services/Car';
 import CarModel from '../models/Car';
 
 const route = Router();
+const path = '/cars';
 
 const car = new CarModel();
 const carService = new CarService(car);
 const carController = new CarController(carService);
 
-route.post('/cars', (req, res) => carController.create(req, res));
-route.get('/cars', (req, res) => carController.read(req, res));
-route.get('/cars/:id', (req, res) => carController.readOne(req, res));
-route.put('/cars/:id', (req, res) => carController.update(req, res));
-route.delete('/cars/:id', (req, res) => carController.delete(req, res));
+route.post(path, (req, res) => carController.create(req, res));
+route.get(path, (req, res) => carController.read(req, res));
+route.get(`${path}/:id`, (req, res) => carController.readOne(req, res));
+route.put(`${path}/:id`, (req, res) => carController.update(req, res));
+route.delete(`${path}/:id`, (req, res) => carController.delete(req, res));
 
 export default route;
